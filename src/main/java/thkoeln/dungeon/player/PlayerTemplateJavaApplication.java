158ff205d6057d6cbdf0d3f9348e7d55000df2ec
Stefan Bente <stefan.bente@th-koeln.de>
2021-11-22 11:31:52 +0100

    empty Java project created

---
+++ src/main/java/thkoeln/dungeon/player/PlayerTemplateJavaApplication.java
@@ -0,0 +1,13 @@
+package thkoeln.dungeon.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerTemplateJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerTemplateJavaApplication.class, args);
+    }
+
+}
