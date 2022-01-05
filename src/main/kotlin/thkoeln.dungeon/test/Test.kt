4839f696c9277887452254eab356dc10470ff108
Regrets <jannik-alx@gmx.de>
2022-01-05 19:18:26 +0100

    Configure Kotlin?

---
+++ src/main/kotlin/thkoeln.dungeon/test/Test.kt
@@ -0,0 +1,11 @@
+package thkoeln.dungeon.test
+
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+class Test(val name: String ="yeet") {
+    @Id
+    val id: UUID = UUID.randomUUID()
+}
