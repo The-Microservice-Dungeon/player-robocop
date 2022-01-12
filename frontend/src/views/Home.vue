eb63ecf2fdaba0d8ae2d6586b6dee49071ebc332
Oliver.Wich <oliverwichmann@outlook.de>
2022-01-12 21:56:21 +0100

    remove default vue stuff

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  mounted() {
+    console.log("mode is " + process.env.NODE_ENV )
+    console.log(process.env.NODE_ENV === 'development')
   }
 }
 </script>
