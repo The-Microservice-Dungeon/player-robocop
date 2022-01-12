1076ba2eea20c9c8b51b7e5d5866e58685d2668b
Oliver.Wich <oliverwichmann@outlook.de>
2022-01-12 21:56:04 +0100

    enable hot reloading of the frontend

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Change build paths to make them Maven compatible
     // see https://cli.vuejs.org/config/
-    outputDir: 'target/dist',
+    outputDir: process.env.NODE_ENV === 'development' ? '../backend/src/main/resources/public' : 'target/dist',
     assetsDir: 'static'
 }
