50131024fdfb97508c9ea0fab3b98d4bf85a0de2
Oliver.Wich <oliverwichmann@outlook.de>
2022-01-13 14:27:11 +0100

    [frontend] add disclaimer

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <img alt="Vue logo" src="../assets/logo_angry.webp">
-    <h1>Authenticate yourself to Robocop Or DIE!</h1>
-    <form>
+    <h1 style="margin-bottom: 0">Authenticate yourself to Robocop Or DIE!</h1>
+    <small>Disclaimer: not authenticating won't actually kill you. You will just be thrown into a tiny cell for the rest of your life.</small>
+    <form style="margin-top: 2rem">
       <input v-model="password" type="password">
       <button @click="authenticate">Authenticate</button>
     </form>
