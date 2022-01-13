6ca7260ba0e35256a1e5ffa5d8b21eff9f14205f
Regrets <jannik-alx@gmx.de>
2022-01-13 19:19:43 +0100

    added kafka errors

---
+++ backend/src/main/java/thkoeln/dungeon/eventconsumer/core/KafkaError.java
@@ -0,0 +1,41 @@
+package thkoeln.dungeon.eventconsumer.core;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+public class KafkaError {
+    @Id
+    @Getter
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Setter
+    @Getter
+    private String topic;
+    @Setter
+    @Getter
+    private String message;
+    @Setter
+    @Getter
+    private String payload;
+
+    public KafkaError(String topic, String message, String payload){
+        this.topic = topic;
+        this.message = message;
+        this.payload = payload;
+    }
+}
---
+++ backend/src/main/java/thkoeln/dungeon/eventconsumer/core/KafkaErrorService.java
@@ -0,0 +1,23 @@
+package thkoeln.dungeon.eventconsumer.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaErrorService {
+    private final KafkaErrorRepository kafkaErrorRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaErrorService.class);
+
+    @Autowired
+    public KafkaErrorService(KafkaErrorRepository kafkaErrorRepository) {
+        this.kafkaErrorRepository = kafkaErrorRepository;
+    }
+
+    public void newKafkaError(String topic, String consumerRecord, String exception) {
+        KafkaError error = new KafkaError(topic, exception, consumerRecord );
+        LOGGER.error("Error processing " +topic + " event."+"\nException: "+exception+"\nPayload: "+consumerRecord);
+        this.kafkaErrorRepository.save(error);
+    }
+}
