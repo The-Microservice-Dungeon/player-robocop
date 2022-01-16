d5409bbc6a495bbb4be7afd9076b3ee135f5235f
Oliver.Wich <oliverwichmann@outlook.de>
2022-01-16 19:41:13 +0100

    [backend] update package for Game Management endpoint

--- backend/src/main/java/thkoeln/dungeon/endpoints/web/game/management/GameManagementController.java
+++ backend/src/main/java/thkoeln/dungeon/endpoints/web/game/management/GameManagementController.java
@@ -1,4 +1,4 @@
-package thkoeln.dungeon.endpoints.web.gamemanagement;
+package thkoeln.dungeon.endpoints.web.game.management;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
