95e6302666ba13826fc4eb3804f6f5e0f1ed3277
Oliver.Wich <oliverwichmann@outlook.de>
2022-01-19 18:11:49 +0100

    [frontend] update eslintrc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es6: true,
   },
   'extends': [
-    'eslint:recommended',
     'plugin:vue/recommended',
     '@vue/standard',
   ],
