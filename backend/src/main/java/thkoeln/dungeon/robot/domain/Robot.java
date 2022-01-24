727e77b58455fbdbf078f215208fe759047acc3d
ThmsRa <65783995+ThmsRa@users.noreply.github.com>
2022-01-24 21:16:39 +0100

    Update Robot.java

--- backend/src/main/java/thkoeln/dungeon/robot/domain/Robot.java
+++ backend/src/main/java/thkoeln/dungeon/robot/domain/Robot.java
@@ -1,25 +1,3 @@
-
-package thkoeln.dungeon.robot.domain;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.util.UUID;
-
-@Entity
-@Getter
-@Setter
-public class Robot {
-    @Id
-    private final UUID id = UUID.randomUUID();
-
-    private int energy;
-    private int health;
-
-}
-
 package thkoeln.dungeon.robot.domain;
 
 import lombok.Getter;
