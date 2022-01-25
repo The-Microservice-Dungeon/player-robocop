514f6cba791893c3b697c7e5f82970ac0e75851d
Oliver.Wich <oliverwichmann@outlook.de>
2022-01-25 20:11:14 +0100

    [frontend] reformat utils

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,30 +1,30 @@
 export const apiLink = (path, params) => {
-    if (!path.startsWith('/')) path = '/' + path
-    let urlString = window.location.protocol + window.location.host + path
-    if (urlString.endsWith('/')) {
-        urlString = urlString.substring(0, urlString.length - 1)
-    }
-    let url = new URL(urlString)
-    if (params !== undefined && Object.keys(params).length > 0) {
-        Object.keys(params).forEach(key => {
-            if (params[key] !== undefined) {
-                url.searchParams.append(key, params[key])
-            }
-        })
-    }
-    return url
+  if (!path.startsWith('/')) path = '/' + path
+  let urlString = window.location.protocol + window.location.host + path
+  if (urlString.endsWith('/')) {
+    urlString = urlString.substring(0, urlString.length - 1)
+  }
+  let url = new URL(urlString)
+  if (params !== undefined && Object.keys(params).length > 0) {
+    Object.keys(params).forEach(key => {
+      if (params[key] !== undefined) {
+        url.searchParams.append(key, params[key])
+      }
+    })
+  }
+  return url
 }
 
 export const apiCall = (method = 'GET', path, params = {}, body = undefined, headers = {}) => {
-    const url = path instanceof URL ? path : apiLink(path, params)
-    return fetch(url, {
-        headers: {
-            ...headers,
-        },
-        method,
-        body,
-    })
-        .catch(console.error)
+  const url = path instanceof URL ? path : apiLink(path, params)
+  return fetch(url, {
+    headers: {
+      ...headers,
+    },
+    method,
+    body,
+  })
+    .catch(console.error)
 }
 
 export const apiPost = (path, params, body, headers) => apiCall('POST', path, params, body, headers)
