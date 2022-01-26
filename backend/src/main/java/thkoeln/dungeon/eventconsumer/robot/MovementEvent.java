3f9ddafd35d5f888974488a8db59ee988363c4fb
Regrets <jannik-alx@gmx.de>
2022-01-26 21:49:18 +0100

    [backend] created MovementEvent

--- backend/src/main/java/thkoeln/dungeon/eventconsumer/robot/MovementEvent.java
+++ backend/src/main/java/thkoeln/dungeon/eventconsumer/robot/MovementEvent.java
@@ -1,24 +1,104 @@
 package thkoeln.dungeon.eventconsumer.robot;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import thkoeln.dungeon.eventconsumer.core.AbstractEvent;
-import thkoeln.dungeon.planet.domain.Planet;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 
+
+import java.util.*;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "success",
+        "message",
+        "remainingEnergy",
+        "planet",
+        "robots"
+})
+@Generated("jsonschema2pojo")
 @Entity
-@Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class MovementEvent extends AbstractEvent {
+
+    @JsonProperty("success")
     private Boolean success;
+    @JsonProperty("message")
     private String message;
-    private int remainingEnergy;
-    @OneToOne
-    private Planet planet;
+    @JsonProperty("remainingEnergy")
+    private Integer remainingEnergy;
+    @JsonProperty("planet")
+    private PlanetMovementDto planet;
+    @JsonProperty("robots")
+    private List<UUID> robots = null;
+
+    @JsonProperty("success")
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    @JsonProperty("success")
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    @JsonProperty("message")
+    public String getMessage() {
+        return message;
+    }
+
+    @JsonProperty("message")
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @JsonProperty("remainingEnergy")
+    public Integer getRemainingEnergy() {
+        return remainingEnergy;
+    }
+
+    @JsonProperty("remainingEnergy")
+    public void setRemainingEnergy(Integer remainingEnergy) {
+        this.remainingEnergy = remainingEnergy;
+    }
+
+    @JsonProperty("planet")
+    public PlanetMovementDto getPlanet() {
+        return planet;
+    }
+
+    @JsonProperty("planet")
+    public void setPlanet(PlanetMovementDto planet) {
+        this.planet = planet;
+    }
+
+    @JsonProperty("robots")
+    public List<UUID> getRobots() {
+        return robots;
+    }
+
+    @JsonProperty("robots")
+    public void setRobots(List<String> robots) {
+        List<UUID> result = new LinkedList<>();
+        for (String robot: robots){
+            result.add(UUID.fromString(robot));
+        }
+        this.robots = result;
+    }
 
 }
---
+++ backend/src/main/java/thkoeln/dungeon/eventconsumer/robot/PlanetMovementDto.java
@@ -0,0 +1,68 @@
+package thkoeln.dungeon.eventconsumer.robot;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.annotation.Generated;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "planetId",
+        "movementDifficulty",
+        "planetType",
+        "resourceType"
+})
+@Generated("jsonschema2pojo")
+public class PlanetMovementDto {
+
+    @JsonProperty("planetId")
+    private String planetId;
+    @JsonProperty("movementDifficulty")
+    private Integer movementDifficulty;
+    @JsonProperty("planetType")
+    private String planetType;
+    @JsonProperty("resourceType")
+    private String resourceType;
+
+    @JsonProperty("planetId")
+    public String getPlanetId() {
+        return planetId;
+    }
+
+    @JsonProperty("planetId")
+    public void setPlanetId(String planetId) {
+        this.planetId = planetId;
+    }
+
+    @JsonProperty("movementDifficulty")
+    public Integer getMovementDifficulty() {
+        return movementDifficulty;
+    }
+
+    @JsonProperty("movementDifficulty")
+    public void setMovementDifficulty(Integer movementDifficulty) {
+        this.movementDifficulty = movementDifficulty;
+    }
+
+    @JsonProperty("planetType")
+    public String getPlanetType() {
+        return planetType;
+    }
+
+    @JsonProperty("planetType")
+    public void setPlanetType(String planetType) {
+        this.planetType = planetType;
+    }
+
+    @JsonProperty("resourceType")
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    @JsonProperty("resourceType")
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+}
