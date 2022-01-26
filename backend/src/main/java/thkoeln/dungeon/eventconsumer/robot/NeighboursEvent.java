18193367bd0d9c878d0b3034f2582b427d702fcf
Regrets <jannik-alx@gmx.de>
2022-01-26 22:04:34 +0100

    [backend] Create NeighboursEvent

---
+++ backend/src/main/java/thkoeln/dungeon/eventconsumer/robot/NeighboursEvent.java
@@ -0,0 +1,57 @@
+package thkoeln.dungeon.eventconsumer.robot;
+
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import thkoeln.dungeon.planet.domain.CompassDirection;
+
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "planetId",
+        "movementDifficulty",
+        "direction"
+})
+@Generated("jsonschema2pojo")
+public class NeighboursEvent {
+
+    @JsonProperty("planetId")
+    private UUID planetId;
+    @JsonProperty("movementDifficulty")
+    private Integer movementDifficulty;
+    @JsonProperty("direction")
+    private CompassDirection direction;
+
+    @JsonProperty("planetId")
+    public UUID getPlanetId() {
+        return planetId;
+    }
+
+    @JsonProperty("planetId")
+    public void setPlanetId(String planetId) {
+        this.planetId = UUID.fromString(planetId);
+    }
+
+    @JsonProperty("movementDifficulty")
+    public Integer getMovementDifficulty() {
+        return movementDifficulty;
+    }
+
+    @JsonProperty("movementDifficulty")
+    public void setMovementDifficulty(Integer movementDifficulty) {
+        this.movementDifficulty = movementDifficulty;
+    }
+
+    @JsonProperty("direction")
+    public CompassDirection getDirection() {
+        return direction;
+    }
+
+    @JsonProperty("direction")
+    public void setDirection(String direction) {
+        this.direction = CompassDirection.valueOf(direction);
+    }
+
+}
