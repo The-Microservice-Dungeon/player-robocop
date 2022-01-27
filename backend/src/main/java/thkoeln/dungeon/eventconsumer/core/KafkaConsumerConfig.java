c48621d533b164251018518d97ce803b2981d10f
Oliver.Wich <oliverwichmann@outlook.de>
2022-01-27 13:26:21 +0100

    [backend] hopefully remove constant config printing by kafka

--- backend/src/main/java/thkoeln/dungeon/eventconsumer/core/KafkaConsumerConfig.java
+++ backend/src/main/java/thkoeln/dungeon/eventconsumer/core/KafkaConsumerConfig.java
@@ -1,8 +1,13 @@
 package thkoeln.dungeon.eventconsumer.core;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,11 +21,47 @@ import java.util.Map;
 
 @EnableKafka
 @Configuration
-public class KafkaConsumerConfig {
+public class KafkaConsumerConfig implements SmartLifecycle {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
+
+    private volatile boolean running = false;
+
+    @Override
+    public boolean isAutoStartup() {
+        return true;
+    }
+
+    @Override
+    public void stop( final @NotNull Runnable callback ) {
+        stop();
+        callback.run();
+    }
+
+    @Override
+    public void start() {
+        Logger rootLogger = (Logger) LoggerFactory.getLogger( ConsumerConfig.class.getName() );
+        rootLogger.setLevel( Level.WARN );
+
+        this.running = true;
+    }
+
+    @Override
+    public void stop() {
+        this.running = false;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public int getPhase() {
+        return Integer.MAX_VALUE - 999;
+    }
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
