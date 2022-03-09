b826f6da923f324902ddad02df0bf65b6e666225
Regrets <jannik-alx@gmx.de>
2022-03-09 13:56:28 +0100

    [backend] aktionismus

--- backend/src/main/java/thkoeln/dungeon/game/domain/game/GameDto.java
+++ backend/src/main/java/thkoeln/dungeon/game/domain/game/GameDto.java
@@ -1,9 +1,11 @@
 package thkoeln.dungeon.game.domain.game;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import thkoeln.dungeon.game.domain.game.GameStatus;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,8 +21,22 @@ public class GameDto {
     private Integer maxPlayers;
     private Integer maxRounds;
     private Integer currentRoundCount;
-    private List<UUID> participatingPlayers;
+    @JsonProperty("participatingPlayers")
+    private List<UUID> participatingPlayers = null;
 
     public GameDto(UUID gameId0, GameStatus created, int i) {
+    }
+    @JsonProperty("participatingPlayers")
+    public List<UUID> getParticipatingPlayers() {
+        return participatingPlayers;
+    }
+
+    @JsonProperty("participatingPlayers")
+    public void setParticipatingPlayers(List<String> participatingPlayers) {
+        List<UUID> result = new LinkedList<>();
+        for (String playerId: participatingPlayers){
+            result.add(UUID.fromString(playerId));
+        }
+        this.participatingPlayers = result;;
     }
 }
